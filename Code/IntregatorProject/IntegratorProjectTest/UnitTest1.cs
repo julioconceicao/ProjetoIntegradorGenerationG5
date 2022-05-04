c4dd146aae6e77de02dfd78156ae39fadbb6ac93
julioconceicao <julioconceicao.gtr@gmail.com>
2022-05-04 11:24:29 -0300

    @julioconceicao [Task: Creating the IntegratorProjectTest]{May.04.2022}

---
+++ Code/IntregatorProject/IntegratorProjectTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegratorProjectTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
