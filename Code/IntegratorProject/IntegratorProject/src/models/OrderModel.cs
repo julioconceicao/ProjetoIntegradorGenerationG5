cef189e9bd0b35c58f98b5b3a86c8c65a835c566
Joaoms98 <jvm1800@gmail.com>
2022-05-04 13:03:58 -0300

    @Joaoms98 [Task: Create OrderModel] {04/05/2022}

--- Code/IntegratorProject/IntegratorProject/src/models/OrderModel.cs
+++ Code/IntegratorProject/IntegratorProject/src/models/OrderModel.cs
@@ -1,6 +1,20 @@
-﻿namespace IntegratorProject.src.models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntegratorProject.src.models
 {
+    [Table("tb_Order")]
     public class OrderModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("Fk_User")]
+        public UserModel User { get; set; }
+
+        [ForeignKey("Fk_Kit")]
+        public KitModel Kit { get; set; }
     }
 }
+
