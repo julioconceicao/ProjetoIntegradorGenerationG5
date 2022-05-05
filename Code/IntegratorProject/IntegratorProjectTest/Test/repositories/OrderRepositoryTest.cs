ee45a9bf738c9e7b16d71c6ab7bb3fe1659dd5fe
LethicyasLopes <lethicyaslopes@gmail.com>
2022-05-05 16:20:24 -0300

    @LethicyasLopes [Task: Created OrderRepositoryTest] {05/05/2022 16:00}

---
+++ Code/IntegratorProject/IntegratorProjectTest/Test/repositories/OrderRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using IntegratorProject.src.data;
+using IntegratorProject.src.repositories;
+using IntegratorProject.src.repositories.implements;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegratorProjectTest.Test.repositories
+{
+    public class OrderRepositoryTest
+    {
+        private IntegratorProjectContext _context;
+        private IOrder _repository;
+
+        [TestInitialize]
+
+        public void InitialSettings()
+        {
+            var opt = new DbContextOptionsBuilder<IntegratorProjectContext>()
+                .UseInMemoryDatabase(databaseName:"db_IntegratorProject")
+                .Options();
+       
+            _context = new IntegratorProjectContext(opt);
+            _repository = new OrderRepository(_context);
+        }
+    }
+}
