8e561927ec065d0efe0b6fc1470b521303024a3b
Mimiocordeiro <101778002+Mimiocordeiro@users.noreply.github.com>
2022-05-11 15:20:07 -0300

    @MichelliOCordeiro [Task: Create AuthenticateDTO]

---
+++ Code/IntegratorProject/IntegratorProject/src/dtos/AuthenticateDTO.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using IntegratorProject.src.utilities;
+
+namespace IntegratorProject.src.dtos
+{
+    public class AuthenticateDTO
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public AuthenticateDTO(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+
+    public class AuthorizationDTO
+    {
+            public int Id { get; set; }
+            public string Email { get; set; }
+            public UserType Type { get; set; }
+            public string Token { get; set; }
+
+            public AuthorizationDTO(int id, string email, UserType type, string token)
+            {
+                Id = id;
+                Email = email;
+                UserType = type;
+                Token = token;
+            }
+    }
+}
