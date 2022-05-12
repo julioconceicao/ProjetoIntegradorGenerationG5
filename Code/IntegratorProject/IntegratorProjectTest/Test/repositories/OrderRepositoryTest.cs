87215d53f7f00695345b824aaf713c850a681442
LethicyasLopes <lethicyaslopes@gmail.com>
2022-05-12 15:44:38 -0300

    @LethicyasLopes [Task: Added Async/Await in OrderRepositoryTest. Test performed.] {12/05/2022 15:44}

--- Code/IntegratorProject/IntegratorProjectTest/Test/repositories/OrderRepositoryTest.cs
+++ Code/IntegratorProject/IntegratorProjectTest/Test/repositories/OrderRepositoryTest.cs
@@ -20,20 +20,20 @@ namespace IntegratorProjectTest.Test.repositories
         [TestInitialize]
         public void InitialSettings()
         {
-            var opt = new DbContextOptionsBuilder<IntegratorProjectContext>()
+             var opt = new DbContextOptionsBuilder<IntegratorProjectContext>()
             .UseInMemoryDatabase(databaseName: "db_IntergratorProject")
             .Options;
-            _context = new IntegratorProjectContext(opt);
-            _repository = new OrderRepository(_context);
+             _context = new IntegratorProjectContext(opt);
+             _repository = new OrderRepository(_context);
         }
 
         [TestMethod]
 
-        public void CreateTwoOrdersReturnTwoOrders()
+        public async Task CreateTwoOrdersReturnTwoOrdersAsync()
         {
-            _repository.AddNewOrder(
+           await _repository.AddNewOrderAsync(
             new NewOrderDTO("grupo5@email.com",1));
-            _repository.AddNewOrder(
+           await _repository.AddNewOrderAsync(
             new NewOrderDTO("generation@email.com",2));
 
             Assert.AreEqual(2, _context.Orders.Count());
