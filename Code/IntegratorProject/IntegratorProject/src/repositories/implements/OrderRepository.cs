d664b76086db123f016b1bba946802b75071165f
LethicyasLopes <lethicyaslopes@gmail.com>
2022-05-13 15:48:13 -0300

    @LethicyasLopes @MatheusRodriguesSilva [Task: Added Order Documentation] {13/05/2022}

--- Code/IntegratorProject/IntegratorProject/src/repositories/implements/OrderRepository.cs
+++ Code/IntegratorProject/IntegratorProject/src/repositories/implements/OrderRepository.cs
@@ -1,6 +1,9 @@
 ﻿using IntegratorProject.src.data;
 using IntegratorProject.src.dtos;
 using IntegratorProject.src.models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,30 +31,57 @@ namespace IntegratorProject.src.repositories.implements
         #endregion Constructors
 
         #region Methods
+
+        /// <summary>
+        /// Add New Order
+        /// </summary>
+        /// <param name="order">OrderDTO</param>
+        /// <returns>ActionResult</returns>
+        /// <response code="201">Order created</response>
+        /// <response code="400">Request error</response>
+        /// <response code="401">Order already created</response>
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(OrderModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [HttpPost]
+        [Authorize(Roles = "ADMIN, USER")]
         public async Task AddNewOrderAsync(NewOrderDTO order)
         {
-           await _context.Orders.AddAsync(new OrderModel
+            await _context.Orders.AddAsync(new OrderModel
             {
-<<<<<<< HEAD
                 Kit = await _context.Kits.FirstOrDefaultAsync(k => k.Id == order.IdKit),
                 User = await _context.Users.FirstOrDefaultAsync(u => u.Email == order.EmailCreator)
             });
-           await _context.SaveChangesAsync();
-=======
-                Kit = _context.Kits.FirstOrDefault(k => k.Id == order.Kit),
-                User = _context.Users.FirstOrDefault(u => u.Email == order.EmailCreator)
-
-            }
-            );
-             await _context.SaveChangesAsync();
->>>>>>> bc5a530440f2fffa41fef3ca482d284f338772f8
+            await _context.SaveChangesAsync();
         }
-            
-        public async Task<List<OrderModel>>GetAllOrdersAsync()
+
+        /// <summary>
+        /// Get all Orders
+        /// </summary>
+        /// <param name="idOrder">int</param>
+        /// <returns>ActionResult</returns>
+        /// <response code="200">Return list???????????</response>
+        /// <response code="204">No Content</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OrderModel))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        [Authorize]
+        public async Task<List<OrderModel>> GetAllOrdersAsync()
         {
             return await _context.Orders.ToListAsync();
         }
 
+        /// <summary>
+        /// Get Order by Id
+        /// </summary>
+        /// <param name="idOrder">int</param>
+        /// <returns>ActionResult</returns>
+        /// <response code="200">Return Order</response>
+        /// <response code="404">Order not found</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OrderModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("id/{idOrder}")]
+        [Authorize]
         public async Task<OrderModel> GetOrderByIdAsync(int id)
         {
             return await _context.Orders.FirstOrDefaultAsync(u => u.Id == id);
@@ -59,3 +89,4 @@ namespace IntegratorProject.src.repositories.implements
         #endregion Methods
     }
 }
+
