eac52ad4f7e71e85ee7c7ae3e427161ad3cd0c69
@AlmeidaaPauloo <paulovictoralmeida97@gmail.com>
2022-05-13 16:16:00 -0300

    @AlmeidaaPauloo [Task: Created Swagger configuration]{13/05/2022 16:15

--- Code/IntegratorProject/IntegratorProject/Startup.cs
+++ Code/IntegratorProject/IntegratorProject/Startup.cs
@@ -15,7 +15,9 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,16 +35,16 @@ namespace IntegratorProject
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }        
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            services.AddDbContext<IntegratorProjectContext>(opt => opt.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));  
-            
-            services.AddScoped<IUser,UserRepository>();
-            services.AddScoped<IOrder,OrderRepository>();
-            services.AddScoped<IKit,KitRepository>();            
+
+            services.AddDbContext<IntegratorProjectContext>(opt => opt.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            services.AddScoped<IUser, UserRepository>();
+            services.AddScoped<IOrder, OrderRepository>();
+            services.AddScoped<IKit, KitRepository>();
 
             services.AddCors();
             services.AddControllers();
@@ -66,8 +68,42 @@ namespace IntegratorProject
                     ValidateAudience = false
                 };
             });
-        }    
+            // Configuração Swagger
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "IntegratorProject", Version = "v1" });
 
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT authorization header utiliza: Bearer + JWT Token",
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            });
+        }
+       
         /// <summary>
         /// <para>Resume> Configure database initialize from context</para>
         /// <para>By: José Vinicius</para>
@@ -79,6 +115,8 @@ namespace IntegratorProject
             if (env.IsDevelopment())
             {
                 context.Database.EnsureCreated();
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
                 app.UseDeveloperExceptionPage();              
             }
 
