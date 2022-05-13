10ee43d121e5a4718648c240ebedd2bf05e53bb7
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-05-13 16:17:00 -0300

    @julioconceicao [Task: DTOs/Controller/Interface documentation] {12/05/2022}

--- Code/IntegratorProject/IntegratorProject/src/controllers/UserController.cs
+++ Code/IntegratorProject/IntegratorProject/src/controllers/UserController.cs
@@ -33,6 +33,32 @@ namespace IntegratorProject.src.controllers
 
         #region Methods
 
+        /// <summary>
+        /// Create a new user
+        /// </summary>
+        /// <param name="user">NewUserDTO</param>
+        /// <returns>ActionResult</returns>
+        /// <remarks>
+        /// Example:
+        ///
+        ///     POST /api/Users
+        ///     {
+        ///        "name": "Julinho Lindo",
+        ///        "cpf_cnpj": "42282758010"
+        ///        "email": "juju@domain.com",
+        ///        "password": "134652",
+        ///        "adress": "Av. Paulista",
+        ///        "NameAgent": " "
+        ///        "type": "NORMAL"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns created user</response>
+        /// <response code="400">Requisition error</response>
+        /// <response code="401">Email already signed in</response>
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(UserModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [HttpPost]
         [AllowAnonymous]
         public async Task<ActionResult> AddNewUserAsync([FromBody] NewUserDTO user)
@@ -48,9 +74,32 @@ namespace IntegratorProject.src.controllers
             {
                 return Unauthorized(ex.Message);
             }
-
         }
 
+        /// <summary>
+        /// Update User
+        /// </summary>
+        /// <param name="user">UpdateUserDTO</param>
+        /// <returns>ActionResult</returns>
+        /// <remarks>
+        /// Example:
+        ///
+        ///     PUT /api/Users
+        ///     {
+        ///        "id": 1,    
+        ///        "nome": "José Vinicius",
+        ///        "cpf_cnpj": "43328758010"
+        ///         "email": "julio@outlook.com"
+        ///         "password": "134652",
+        ///        "adress": "Mauá"
+        ///         "nameAgent": " "
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Returns updated user</response>
+        /// <response code="400">Requisition error</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut]
         [Authorize(Roles = "USER, ADMIN, ONG")]
         public async Task<ActionResult> UpdateUserAsync([FromBody] UpdateUserDTO user)
@@ -61,6 +110,13 @@ namespace IntegratorProject.src.controllers
                 return Ok(user);
         }
 
+        /// <summary>
+        /// Delete user by Id
+        /// </summary>
+        /// <param name="idUser">int</param>
+        /// <returns>ActionResult</returns>
+        /// <response code="204">User deleted</response>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [HttpDelete("delete/{idUser}")]
         [Authorize(Roles = "ADMIN")]
         public async Task<ActionResult> DeleteUserAsync([FromRoute] int idUser)
@@ -69,6 +125,12 @@ namespace IntegratorProject.src.controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Get all Ongs
+        /// </summary>
+        /// <returns>ActionResult</returns>
+        /// <response code="200">Returns list</response>
+        /// <response code="204">Empty list</response>
         [HttpGet]
         [Authorize(Roles = "USER, ADMIN")]
         public async Task<ActionResult> GetAllOngsAsync()
@@ -78,6 +140,15 @@ namespace IntegratorProject.src.controllers
             return Ok(list);
         }
 
+        /// <summary>
+        /// Get User By Address
+        /// </summary>
+        /// <param name="adress">string</param>
+        /// <returns>ActionResult</returns>
+        /// <response code="200">Returns user</response>
+        /// <response code="404">Email not found</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("adress/{userAdress}")]
         [Authorize(Roles = "USER, ONG, ADMIN")]
         public async Task<ActionResult> GetUserByAdressAsync([FromRoute] string adress)
@@ -88,6 +159,15 @@ namespace IntegratorProject.src.controllers
                 return Ok(user);
         }
     
+        /// <summary>
+        /// Get User By Id
+        /// </summary>
+        /// <param name="idUser">int</param>
+        /// <returns>ActionResult</returns>
+        /// <response code="200">Returns user</response>
+        /// <response code="404">User not found</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("id/{idUser}")]
         [Authorize(Roles = "USER, ONG, ADMIN")]
         public async Task<ActionResult> GetUserByIdAsync([FromRoute]int idUser)
