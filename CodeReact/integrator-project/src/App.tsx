89295a59953015c67ffbe223dbba76c21f7db94d
Joaoms98 <jvm1800@gmail.com>
2022-06-01 17:02:41 -0300

    @Joaoms98 [Task: Criado base do frontend do projeto e criado o home da página] {01/06/2021 17:02}

---
+++ CodeReact/integrator-project/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/statics/navbar/Navbar';
+import Home from './Components/pages/home/Home'
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
