b9ef722c20f764c625eaac29d2a85bca40456705
Lethicya Lopes <lethicyaslopes@gmail.com>
2022-06-02 16:25:25 -0300

    @LethicyasLopes [Task: Registration page created] {02/06/2022 16:25}

--- CodeReact/integrator-project/src/App.tsx
+++ CodeReact/integrator-project/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/statics/navbar/Navbar';
+import Registration from '../src/Components/pages/registration/Registration';
 import Home from './Components/pages/home/Home'
 import Footer from './Components/statics/footer/Footer'
 import Login from './Components/pages/login/Login';
@@ -9,19 +10,20 @@ import Login from './Components/pages/login/Login';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/login'>
-            <Login/>
-          </Route>
-        </Switch>
-      </div>
-        <Footer />
-    </Router>
+    <>
+      <Router>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            
+            <Route path='/login' element={ <Login />} />
+
+            <Route path='/cadastro' element={ <Registration />} />
+             
+          </Routes>
+          <Footer />
+      </Router >
+    </>
   );
 }
 export default App;
