cfe139d8d84e752581e9f2e74b88223dc33d52df
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-02 11:44:47 -0300

    [Task: Created Footer] {02/06/2022}

--- CodeReact/integrator-project/src/App.tsx
+++ CodeReact/integrator-project/src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/statics/navbar/Navbar';
 import Home from './Components/pages/home/Home'
+import Footer from './Components/statics/footer/Footer'
+
 
 function App() {
   return (
     <>
+      
       <Home />
+      <Footer />
+      
     </>
   );
 }
