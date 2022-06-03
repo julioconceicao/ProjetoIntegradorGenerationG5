7a2514e5998e61ae0aaac4af7f4d515b48fa4a4f
Mimiocordeiro <101778002+Mimiocordeiro@users.noreply.github.com>
2022-06-03 17:31:46 -0300

    @MichelliOCordeiro [Task: Created Service, installed the Axios library and created the About Us page] {03/06/2022 17h30}

--- CodeReact/integrator-project/src/App.tsx
+++ CodeReact/integrator-project/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/statics/navbar/Navbar';
 import Home from './Components/pages/home/Home'
+import AboutUs from './Components/pages/aboutUs/AboutUs'
 
 function App() {
   return (
     <>
       <Home />
+        <AboutUs />
     </>
   );
 }
---
+++ CodeReact/integrator-project/src/Components/pages/aboutUs/AboutUs.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {Typography, Box, Grid, Button} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import "./AboutUs.css";
+
+function AboutUs() {
+    return (
+      <>
+        
+        <div className='body'>
+          <Typography variant="h3" gutterBottom color="textPrimary" component="h1" text-align="center" className='titulo'>Sobre nós</Typography>
+            <section className='card'>
+              <img src="https://i.imgur.com/VRckNLU.png" alt="Missão" width="300px" height="120px" padding-left="20px" />
+            <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='subtitulo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, libero optio eum voluptatibus consectetur expedita aliquam hic. Nihil maxime nobis doloribus perferendis distinctio tempora iusto ullam! Atque obcaecati quasi nam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, libero optio eum voluptatibus consectetur expedita aliquam hic. Nihil maxime nobis doloribus perferendis distinctio tempora iusto ullam! Atque obcaecati quasi nam.</Typography>
+          </section>
+
+             
+          <section className='card'>
+              <img src="https://i.imgur.com/S48xRO9.png" alt="Missão" width="200px" height="120px" padding-left="20px" />
+            <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='subtitulo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, libero optio eum voluptatibus consectetur expedita aliquam hic. Nihil maxime nobis doloribus perferendis distinctio tempora iusto ullam! Atque obcaecati quasi nam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, libero optio eum voluptatibus consectetur expedita aliquam hic. Nihil maxime nobis doloribus perferendis distinctio tempora iusto ullam! Atque obcaecati quasi nam.</Typography>
+          </section>
+
+          <section className='card'>
+              <img src="https://i.imgur.com/QjfdjZv.png" alt="Missão" width="150px" height="120px" padding-left="20px" />
+            <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='subtitulo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, libero optio eum voluptatibus consectetur expedita aliquam hic. Nihil maxime nobis doloribus perferendis distinctio tempora iusto ullam! Atque obcaecati quasi nam.Lorem ipaxime nobis doloribus perferendis distinctio tempora iusto ullam! Atque obcaecati quasi nam.</Typography>
+          </section>
+        
+        </div>
+        
+    </>
+  );
+}
+
+export default AboutUs;
