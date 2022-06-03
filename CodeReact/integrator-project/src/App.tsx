818044651ca455d059f3d9eee7dbeed394139449
@AlmeidaaPauloo <paulovictoralmeida97@gmail.com>
2022-06-02 21:35:00 -0300

    @AlmeidaaPauloo [Task: Remove page kit off app.tsx]{02/06/2022 18:00}

--- CodeReact/integrator-project/src/App.tsx
+++ CodeReact/integrator-project/src/App.tsx
@@ -3,22 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/statics/navbar/Navbar';
 import Registration from '../src/Components/pages/registration/Registration';
-import Home from './Components/pages/home/Home'
-<<<<<<< HEAD
-import Kit from './pages/kit/Kit';
-=======
+import Home from './Components/pages/home/Home';
 import Footer from './Components/statics/footer/Footer'
 import Login from './Components/pages/login/Login';
 
->>>>>>> 78c7f49112cd7e917ed404b6ed361aacd8be884f
+
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
+
       <Home />
-      <Kit />
-=======
+      
       <Router>
 
           <Routes>
@@ -31,7 +27,7 @@ function App() {
           </Routes>
           <Footer />
       </Router >
->>>>>>> 78c7f49112cd7e917ed404b6ed361aacd8be884f
+
     </>
   );
 }
