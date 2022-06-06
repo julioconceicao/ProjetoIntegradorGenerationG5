0db437223b27167610fe48f108cd3d3f03f35c12
Joaoms98 <jvm1800@gmail.com>
2022-06-06 17:00:54 -0300

    @Joaoms98 222

--- CodeReact/integrator-project/src/App.tsx
+++ CodeReact/integrator-project/src/App.tsx
@@ -10,17 +10,20 @@ import Team from './pages/team/Team';
 import Kit from './pages/kit/Kit';
 import AboutUs from './pages/aboutUs/AboutUs';
 import ListOrder from './Components/orders/listOrder/ListOrder';
+import Home2gambiarra from './pages/home/Home2gambiarra'
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-        <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/home' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/cadastro' element={<Registration />} />
           <Route path='/order' element={<ListOrder />} />
+          <Route path='/Home2gambiarra' element={<Home2gambiarra />} />
+          <Route path='/sobrenos' element={<AboutUs />} />
         </Routes>
         <Footer />
       </Router >
