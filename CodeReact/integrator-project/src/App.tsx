5cdb10586aa908b9502e786532a129c66a243481
leticiazimerer <leticiazimerer@yahoo.com.br>
2022-06-06 10:51:39 -0300

    @leticiazimerer [Task: Modify Folders] {06/06/2022 10:50}

--- CodeReact/integrator-project/src/App.tsx
+++ CodeReact/integrator-project/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/statics/navbar/Navbar';
-import Registration from '../src/Components/pages/registration/Registration';
-import Home from './Components/pages/home/Home';
+import Registration from './pages/registration/Registration';
+import Home from './pages/home/Home';
 import Footer from './Components/statics/footer/Footer'
-import Login from './Components/pages/login/Login';
-import Team from './Components/pages/team/Team';
+import Login from './pages/login/Login';
+import Team from './pages/team/Team';
 import Kit from './pages/kit/Kit';
-import AboutUs from './Components/pages/aboutUs/AboutUs';
+import AboutUs from './pages/aboutUs/AboutUs';
 
 function App() {
   return (
