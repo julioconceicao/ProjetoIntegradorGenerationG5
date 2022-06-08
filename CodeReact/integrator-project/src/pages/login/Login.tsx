766a9edd4de14d8add5539bc0b31eb5f144e0352
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-08 13:19:53 -0300

    @julioconceicao [Task: Fixing Merge Conflicts] {08/06/2022}

--- CodeReact/integrator-project/src/pages/login/Login.tsx
+++ CodeReact/integrator-project/src/pages/login/Login.tsx
@@ -59,12 +59,7 @@ function Login() {
               <TextField
                 value={loginDTO.password}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-<<<<<<< HEAD
-                id='password' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
-=======
                 id='password' label='password' variant='outlined' name='password' margin='normal' type='password' fullWidth />
->>>>>>> 249d45151f50c033762c35dabe7fdadee5acbc98
-
               <Box marginTop={2} textAlign='center'>
                 <Button type='submit' variant='contained' color='primary'>
                   Logar
