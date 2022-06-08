638b2b1ca2d83e7a9d78eaab6c134b88e951c7ed
Joaoms98 <jvm1800@gmail.com>
2022-06-08 14:33:05 -0300

    @Joaoms98 [Task: create about us page] {08/06/2022 14:32}

--- CodeReact/integrator-project/src/pages/login/Login.tsx
+++ CodeReact/integrator-project/src/pages/login/Login.tsx
@@ -59,7 +59,7 @@ function Login() {
               <TextField
                 value={loginDTO.password}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                id='password' label='senha' variant='outlined' name='password' margin='normal' type='password' fullWidth />
 
               <Box marginTop={2} textAlign='center'>
                 <Button type='submit' variant='contained' color='primary'>
