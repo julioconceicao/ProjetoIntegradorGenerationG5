458e928daa88244adb2ab80a947460556d4294ed
José Vinicius <vinicius123.vs58@gmail.com>
2022-06-14 23:34:19 -0300

    @Josevinicius53

--- CodeReact/integrator-project/src/pages/ongs/Ong.tsx
+++ CodeReact/integrator-project/src/pages/ongs/Ong.tsx
@@ -1,8 +1,7 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
-import { Typography, Box, Grid, Button, TextField } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom'
+import { Typography, Box, Grid, TextField } from '@mui/material';
 import MenuSidebar from "../../Components/statics/menuSidebar/MenuSidebar";
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 import MenuItem from '@material-ui/core/MenuItem';
 import Home from '../../pages/home/Home';
 import "./Ong.css";
@@ -15,40 +14,54 @@ const currencies = [
   },
 ];
 
-  function Ong() {
-    const [currency, setCurrency] = React.useState('EUR');
-  
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setCurrency(event.target.value);
-    };
-    return (
-      <>
-        <Grid item xs={12} style={{
-          background: `url(https://i.imgur.com/X7h4cHt.png)`,
-          backgroundRepeat: 'no-repeat', width: '100%', height: '100vh', backgroundSize: 'cover'
-        }}>
-          <div className="navbarmenu">
-            <MenuSidebar />
-          </div>
-          <div>
-            <TextField   
-              select   
-              value={currency}
-              onChange={handleChange}
-              helperText="Please select your Kit"
-              variant="outlined"
-              className="select"
-            >
-              {currencies.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </div>  
-        </Grid>
-      </>
-    );
+function Ong() {
+
+  const [currency, setCurrency] = React.useState('EUR');
+  const [token, setToken] = useLocalStorage('token');
+  const [id, setId] = useLocalStorage('id');
+
+  let navigate = useNavigate();
+
+  function goLogout() {
+    setToken('')
+    setId('')
+    alert("Usuário deslogado")
+    navigate('/login')
   }
-  export default Ong;
- 
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrency(event.target.value);
+  };
+  return (
+    <>
+      <Grid item xs={12} style={{
+        background: `url(https://i.imgur.com/X7h4cHt.png)`,
+        backgroundRepeat: 'no-repeat', width: '100%', height: '100vh', backgroundSize: 'cover'
+      }}>
+        <div className="navbarmenu">
+          <MenuSidebar />
+        </div>
+        <div>
+          <TextField
+            select
+            value={currency}
+            onChange={handleChange}
+            helperText="Please select your Kit"
+            variant="outlined"
+            className="select"
+          >
+            {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <div>
+            <button onClick={goLogout} className="navbarbutton1"> Logout</button>
+          </div>
+        </div>
+      </Grid>
+    </>
+  );
+}
+export default Ong;
