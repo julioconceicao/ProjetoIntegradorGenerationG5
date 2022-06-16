dafc6a920027251a03cdcde5f3c80ce1a0acb232
José Vinicius <vinicius123.vs58@gmail.com>
2022-06-16 20:27:37 -0300

    @Josevinicius[Task: Estilizando paginas Home Login Cadastro {16/06/2022}]

--- CodeReact/integrator-project/src/pages/home/Home.tsx
+++ CodeReact/integrator-project/src/pages/home/Home.tsx
@@ -14,37 +14,21 @@ function Home() {
                 background: `url(https://i.imgur.com/9ayuO27.jpg)`,
                 backgroundRepeat: 'no-repeat', width: '100%', height: '100vh', backgroundSize: 'cover'
             }}>
-<<<<<<< HEAD
-                <div className="navbarmenu">
-                    <MenuSidebar />
-                    <div className="navbarbutton">
-                        <Link to='/login'>
-                            <button className="distancia">Login</button>
-                        </Link>
-                    </div>
-                    
-                        <Link to='/Cadastro'>
-                            <button>Cadastre-se</button>
-                        </Link>
-                    
-                </div>
-=======
-                <AppBar position="sticky" style={{ background: 'transparent', boxShadow: 'none'}}>
+                <AppBar position="sticky" style={{ background: 'transparent', boxShadow: 'none' }}>
                     <div className="navbarmenu">
                         <MenuSidebar />
                         <div className="navbarbutton">
                             <Link to='/login'>
-                                <button> Login</button>
-                            </Link>
-                            <Link to='/Cadastro'>
-                                <button>Cadastre-se</button>
+                                <button className="distancia">Login</button>
                             </Link>
                         </div>
+                        <Link to='/Cadastro'>
+                            <button>Cadastre-se</button>
+                        </Link>
                     </div>
                 </AppBar>
->>>>>>> 29657257eb51f1d60ae793a22d01fbcb1a29951d
                 <div>
-                    <img className="logo" src="https://imgur.com/fqAed38.png" alt="" width="250" />
+                    <img src="https://imgur.com/fqAed38.png" alt="" width="250" className="logo" />
                 </div>
             </Grid>
         </>
