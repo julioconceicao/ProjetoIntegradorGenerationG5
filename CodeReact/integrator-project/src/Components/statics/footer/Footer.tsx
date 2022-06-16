4186154821360f051a4f40b7fc7d6cda60e0a498
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-16 09:49:13 -0300

    @julioconceicao [Footer Background]

--- CodeReact/integrator-project/src/Components/statics/footer/Footer.tsx
+++ CodeReact/integrator-project/src/Components/statics/footer/Footer.tsx
@@ -10,29 +10,34 @@ function Footer() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#C49C69", height: "80px" }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h6" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://linktr.ee/g5_integratorproject" target="_blank" rel="noopener noreferrer">
-                                <EmailIcon style={{ fontSize: 40, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ backgroundColor: "#C49C69", height: "60px" }}>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >Todos os direitos reservados: Planeta Solidário © 2022</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://trello.com/b/ewpErhaM/projetointegrador" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Trello</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+        <Grid alignItems="center" item xs={12}>
+            <Box className='box1'>
+                <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                    <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                </Box>
+                <Box display="flex" alignItems="center" justifyContent="center">
+  
+                  
+                    <a href="https://linktr.ee/g5_integratorproject" target="_blank">
+                        <GitHubIcon className='redes' />
+                    </a>
+                    <a href="https://linktr.ee/g5_integratorproject" target="_blank">
+                        <LinkedInIcon className='redes' />
+                    </a>
+                </Box>
+            
+            
+                <Box paddingTop={1}>
+                    <Typography variant="subtitle2" align="center" gutterBottom className='textos'>© 2022 Copyright: Planeta Solidário - Projeto Integrador</Typography>
+                </Box>
+                <Box>
+                    <a target="_blank" href="https://brasil.generation.org">
+                        <Typography variant="subtitle2" gutterBottom className='textos' align="center">brasil.generation.org</Typography>
+                    </a>
+                </Box>
+            </Box>
+        </Grid>
+    </Grid>
         </>
     )
 }
