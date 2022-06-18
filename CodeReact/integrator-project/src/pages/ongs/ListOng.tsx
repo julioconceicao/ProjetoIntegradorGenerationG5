0b2a43452b705744582d998011672ca2f65ec6ed
Joaoms98 <jvm1800@gmail.com>
2022-06-18 15:54:25 -0300

    @Joaoms98 [Task: incluido a listagem de orders no doações recebidas] {18/06/2022 15:44}

--- CodeReact/integrator-project/src/pages/ongs/ListOng.tsx
+++ CodeReact/integrator-project/src/pages/ongs/ListOng.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { Link, useNavigate } from 'react-router-dom'
 import { Typography, Box, Grid, TextField } from '@mui/material';
-import { Card, CardActions, CardContent, Button } from '@material-ui/core';
+import { Card, CardActions, CardContent, Button, AppBar } from '@material-ui/core';
 import MenuSidebar from "../../Components/statics/menuSidebar/MenuSidebar";
 import MenuItem from '@material-ui/core/MenuItem';
 import "./Ong.css";
@@ -54,23 +54,32 @@ function ListOng() {
   }, [type])
 
   return (
-    <>
+    <> <Grid item xs={12} style={{
+      background: `url(https://i.imgur.com/9ayuO27.jpg)`,
+      backgroundRepeat: 'no-repeat', width: '100%', height: '100vh', backgroundSize: 'cover'
+  }}>
+      <AppBar position="sticky" style={{ background: 'transparent', boxShadow: 'none' }}>
+          <div className="navbarmenu">
+              <MenuSidebar />
+          </div>
+      </AppBar>
       {
         orders.map(unit => (
           <Box m={2} >
             <Card variant="outlined">
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>
-                  Compras
+                  Compra
                 </Typography>
                 <Typography variant="body2" component="p">
-                  {unit.id}
+                  Id:{unit.id}
                 </Typography>
               </CardContent>
             </Card>
           </Box>
         ))
       }
+      </Grid>
     </>
   );
 }
